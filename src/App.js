c3340644fd0485332d16f165d34fff88398b8d9c
charlessinhh <charleskumar334@gmail.com>
2023-08-11 01:11:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Input from './Components/Input';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button>I'm a button</button>
+      {<Input/>}
     </div>
+    
   );
 }
 
---
+++ src/Components/Input.js
@@ -0,0 +1,100 @@
+import React, {useState} from 'react';
+
+function Input(){
+
+  const [input1, setInput1] = useState('');
+  const [input2, setInput2] = useState('');
+  const [operationCount, setOperationCount] = useState(0);
+  const [operator, setOperator] = useState('+');
+  const [result, setResult] = useState('');
+
+
+  //handleInputChange1
+  const handleInputChange1 = (event) =>{
+    setInput1(event.target.value);
+  }
+
+
+  // handleInputChange2
+  const handleInputChange2 = (event) => {
+    setInput2(event.target.value);
+  }
+
+  //handleOperatorChange
+  const handleOperatorChange = (selectedOperator) => {
+    setOperator(selectedOperator);
+  }
+
+  //handleCalculate
+  const handleCalculate = () =>{
+    const num1 = parseFloat(input1);
+    const num2 = parseFloat(input2);
+
+    if(!isNaN(num1) && !isNaN(num2)){
+      let calculateResult = 0;
+
+      switch(operator){
+        case '+':
+          calculateResult = num1+num2;
+          break;
+
+        case '-':
+          calculateResult =num1-num2;
+          break;
+
+        case '*':
+          calculateResult = num1*num2;
+          break;
+
+        case '%':
+          calculateResult = num1%num2;
+          break;
+        
+        default:
+          return;
+      }
+
+      //set result with calculated result
+      setResult(calculateResult);
+
+      //increase the operation count
+      setOperationCount(operationCount + 1);
+
+    }; 
+  }
+
+  const handleReset = () => {
+    setInput1('');
+    setInput2('');
+    setResult('');
+    setOperator('+');
+  }
+
+
+  return (
+    <div>
+      <h2>Simple Calculator</h2>
+      <div>
+        <p>Operation Count: {operationCount} </p>
+      </div>
+      <input type="number" placeholder="Input 1" onChange={handleInputChange1} value = {input1} />
+      <span> {operator} </span>
+      <input type="number" placeholder="Input 2"  onChange={handleInputChange2} value = {input2}/>
+
+      <div>
+        <button onClick={ () => handleOperatorChange('+')}>+</button>
+        <button onClick={ () => handleOperatorChange('-')}>-</button>
+        <button onClick={ () => handleOperatorChange('*')}>*</button>
+        <button onClick={ () => handleOperatorChange('%')}>%</button>
+        <button onClick={handleCalculate}>Calculate</button>
+      </div>
+
+      {<p>Result: {result} </p>}
+      <button onClick={ handleReset }>Reset</button>
+    
+    </div>
+  );
+
+};
+
+export default Input;
